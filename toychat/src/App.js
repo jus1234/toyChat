8fe46a443552244f1a9f3354f9ff7ed2af028b9c
jus1234 <osjamg1996@gmail.com>
2022-01-16 18:12:21 +0900

    git push test

--- toychat/src/App.js
+++ toychat/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Git push test
         </a>
       </header>
     </div>
